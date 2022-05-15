8a8dcd5dc5f9286f379f046223da9e705319affc
Galding <nathan.peyronnet@gmail.com>
2022-05-15 16:07:02 +0200

    create the base game

--- puissance4.DesktopClient/Game1.cs
+++ puissance4.DesktopClient/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace puissance4.DesktopClient
 {
@@ -8,20 +9,22 @@ namespace puissance4.DesktopClient
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Puissance4Object cadre;
+        private Puissance4Object white;
         private Puissance4Object red;
         private Puissance4Object yellow;
+        private Puissance4Object arrow;
         const int VX = 6;
         const int VY = 7;
         private byte[,] map;
-        private string direction;
+        private int currentPlayer = 1;
+        private int currentColumn = VY / 2;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            map = new byte[VX, VY]{
+            map = new byte[VX, VY]{ // 0 = empty (white), 1 = yellow, 2 = red
              {0, 0, 0, 0, 0, 0, 0},
              {0, 0, 0, 0, 0, 0, 0},
              {0, 0, 0, 0, 0, 0, 0},
@@ -42,34 +45,47 @@ namespace puissance4.DesktopClient
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
             _graphics.PreferredBackBufferWidth = 1024;
-            _graphics.PreferredBackBufferHeight = 660;
+            _graphics.PreferredBackBufferHeight = 720;
             _graphics.ApplyChanges();
             // on charge un objet mur
-            cadre = new Puissance4Object(Content.Load<Texture2D>("white"), new Vector2(0f,
+            white = new Puissance4Object(Content.Load<Texture2D>("white"), new Vector2(0f,
            0f), new Vector2(100f, 100f));
             yellow = new Puissance4Object(Content.Load<Texture2D>("yellow"), new
            Vector2(0f, 0f), new Vector2(100f, 100f));
             red = new Puissance4Object(Content.Load<Texture2D>("red"), new
            Vector2(0f, 0f), new Vector2(100f, 100f));
+            arrow = new Puissance4Object(Content.Load<Texture2D>("arrow"), new Vector2(0f,
+           0f), new Vector2(100f, 100f));
 
+        }
+
+        private KeyboardState currentKeyState;
+        private KeyboardState previousKeyState = new KeyboardState();
+        protected bool IsKeyPressed(Keys key)
+        {
+            return currentKeyState.IsKeyDown(key) && !previousKeyState.IsKeyDown(key);
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            KeyboardState keyboard = Keyboard.GetState();
-            if (keyboard.IsKeyDown(Keys.Right))
+            this.currentKeyState = Keyboard.GetState();
+            if (IsKeyPressed(Keys.Right) && this.currentColumn < VX)
             {
-                direction = "RIGHT";
+                this.currentColumn++;
             }
-            else if (keyboard.IsKeyDown(Keys.Left))
+            if (IsKeyPressed(Keys.Left) && this.currentColumn > 0)
             {
-                direction = "LEFT";
+                this.currentColumn--;
             }
-            // TODO: Add your update logic here
+            if (IsKeyPressed(Keys.Down))
+            {
+                bool success = this.dropCoin(this.currentColumn, this.currentPlayer);
+                if (success) this.switchPlayer();
+            }
+            this.previousKeyState = this.currentKeyState;
 
             base.Update(gameTime);
         }
@@ -82,31 +98,55 @@ namespace puissance4.DesktopClient
 
             _spriteBatch.Begin();
             // TODO: Add your drawing code here
-            int offsetX = 40;
-            int offsetY = 140;
+            int offsetX = 100;
+            int offsetY = 162;
+            Vector2 arrow_pos = new Vector2(offsetY + this.currentColumn * 100, offsetX - 100);
+            _spriteBatch.Draw(arrow.Texture, arrow_pos, this.currentPlayer == 1 ? Color.Yellow : Color.Red);
             for (int x = 0; x < VX; x++)
             {
                 for (int y = 0; y < VY; y++)
                 {
-                    if (map[x, y] == 0)
+                    int xpos, ypos;
+                    xpos = offsetX + x * 100;
+                    ypos = offsetY + y * 100;
+                    Vector2 pos = new Vector2(ypos, xpos);
+                    switch (map[x, y])
                     {
-                        int xpos, ypos;
-                        xpos = offsetX + x * 100;
-                        ypos = offsetY + y * 100;
-                        Vector2 pos = new Vector2(ypos, xpos);
-                        if (x == 0 && y == 0)
-                        {
+                        case 0: // white
+                            _spriteBatch.Draw(white.Texture, pos, Color.White);
+                            break;
+                        case 1: // yellow
+                            _spriteBatch.Draw(yellow.Texture, pos, Color.White);
+                            break;
+                        case 2: // red
                             _spriteBatch.Draw(red.Texture, pos, Color.White);
-                            map[x, y] = 2;
-                        }
-                        else
-                            _spriteBatch.Draw(cadre.Texture, pos, Color.White);
+                            break;
+                        default:
+                            break;
                     }
                 }
+
             }
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        protected void switchPlayer()
+        {
+            this.currentPlayer = this.currentPlayer == 1 ? 2 : 1;
+        }
+
+        protected bool dropCoin(int column, int player)
+        {
+            if (this.map[0, column] != 0) // column already full
+                return false;
+
+            int x = VX - 1;
+            while (this.map[x, column] != 0) // found first empty from the bottom
+                x--;
+            this.map[x, column] = Convert.ToByte(player);
+            return true;
         }
     }
 }
