692ad9a4451d6135e804fe79c109e88ff4884a97
Galding <nathan.peyronnet@gmail.com>
2022-05-26 15:02:30 +0200

    add enum file

---
+++ puissance4.DesktopClient/Enums.cs
@@ -0,0 +1,15 @@
+namespace puissance4.DesktopClient
+{
+    public enum State
+    {
+        Menu,
+        Playing,
+        End
+    }
+
+    public enum Gamemode
+    {
+        Player,
+        AI
+    }
+}
